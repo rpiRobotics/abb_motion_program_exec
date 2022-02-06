4b95d11ed25b32958900242b37d268269b765455
John Wason <wason@wasontech.com>
2022-02-05 21:56:27 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='abb_motion_program_exec',
+    version='0.0.2',
+    description='Simple module to execute motion commands on ABB robots and log results',
+    url='https://github.com/johnwason/abb_motion_program_exec',
+    py_modules=['abb_motion_program_exec_client'],
+    install_requires=[
+        'bs4',
+        'requests',
+        'numpy'
+    ]
+)
